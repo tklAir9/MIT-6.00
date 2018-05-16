36ed3c12c09d0c9f0722a290fe86634db30895b5
Uzair Hasan <uzairhasan12@hotmail.com>
2018-05-16 16:35:19 -0400

    Update ps0.py

--- 6.001/ps0.py
+++ 6.001/ps0.py
@@ -1,10 +1,15 @@
+# Problem Set 0
+# Name: Uzair Hasan
+# Collaborators: None
+# Date & Time: May 16, 2018, 4:29 - 4:34
+
 ##Created by Uzair
 ##Objective of this code is to store and return someone's name
 
-lastName = input('Enter your last name: \n**') ##Prompt and store last name
-firstName = input('Enter your first name:\n**' ) ##Prompt and store first name
-print(firstName)##Print firstName 
-print(lastName) ##Print lastName
+lastName = input('Enter your last name: \n**') #Prompt and store last name
+firstName = input('Enter your first name:\n**' ) #Prompt and store first name
+print(firstName)#Print firstName 
+print(lastName) #Print lastName
 
 ##Any questions, feel free to email me and ask!
 ##uzairhasan12@hotmail.com
