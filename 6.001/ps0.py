ec809afbe4b7002e6947c8f44bfe8b05a24c23b6
Uzair Hasan <uzairhasan12@hotmail.com>
2018-05-16 16:32:24 -0400

    Update ps0.py

--- 6.001/ps0.py
+++ 6.001/ps0.py
@@ -3,7 +3,8 @@
 
 lastName = input('Enter your last name: \n**') ##Prompt and store last name
 firstName = input('Enter your first name:\n**' ) ##Prompt and store first name
-print(firstName, '\n', lastName) ##Print firstName and lastName
+print(firstName)##Print firstName 
+print(lastName) ##Print lastName
 
 ##Any questions, feel free to email me and ask!
 ##uzairhasan12@hotmail.com
