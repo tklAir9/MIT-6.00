9e18a894509c9adff0b41f9766d262a05545b010
Uzair Hasan <uzairhasan12@hotmail.com>
2018-05-16 13:55:52 -0400

    Create ps0.py

---
+++ 6.001/ps0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 13:42:31 2018
+
+@author: Uzair Hasan ☺
+"""
+import math
+
+xString = input('Enter number x:')
+yString= input('Enter number y: ')
+x=int(xString)
+y=int(yString)
+print('x**y= ', x**y)
+print('log(x) = ', int(math.log(x,2)))
